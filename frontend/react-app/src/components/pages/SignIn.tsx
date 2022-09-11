c6c94e64db02afd4e8db85f5ecfa254e938f8239
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-12 02:15:19 +0900

    create certificateion_form

--- frontend/react-app/src/components/pages/SignIn.tsx
+++ frontend/react-app/src/components/pages/SignIn.tsx
@@ -1,8 +1,94 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { useNavigate, Link } from "react-router-dom"
+import { Card, CardHeader, CardContent, TextField, Button, Box, Typography } from "@mui/material"
+import Cookies from "js-cookie"
+import AlertMessage from "../utils/AlertMessage"
+import { SignInParams } from "../../interface"
+import { signIn } from "../../lib/api/auth"
+
 
 const SignIn: React.FC = () => {
-  return(
-    <>signin page</>
+  const navigate = useNavigate()
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [alertMessageOpen, setAlertMessageOpen] = useState<boolean>(false)
+
+  const handleSubmit = async(e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+  }
+
+  const handleEasySubmit = async(e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <form noValidate autoComplete="off">
+        <Card sx={{ padding: 2, maxWidth: 400 }}>
+          <CardHeader sx={{ textAlign: "center" }} title="Sign In" />
+          <CardContent>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="Email"
+              value={email}
+              margin="dense"
+              onChange={event => setEmail(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              placeholder="At least 6 characters"
+              value={password}
+              margin="dense"
+              autoComplete="current-password"
+              onChange={event => setPassword(event.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              color="inherit"
+              disabled={!email || !password ? true : false}
+              sx={{ marginTop: 2, flexGrow: 1, textTransform: "none"}}
+              onClick={handleSubmit}
+            >
+              ログイン
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              color="inherit"
+              sx={{ marginTop: 2, flexGrow: 1, textTransform: "none"}}
+              onClick={handleEasySubmit}
+            >
+              簡単ログイン
+            </Button>
+            <Box textAlign="center" sx={{ marginTop: "2rem"}}>
+              <Typography variant="body2">
+                Don't have an account? &nbsp;
+                <Link to="/signup" style={{ textDecoration: "none" }}>
+                  Sign Up now!
+                </Link>
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </form>
+      <AlertMessage
+        open={alertMessageOpen}
+        setOpen={setAlertMessageOpen}
+        severity="error"
+        message="Invalid email or password"
+      />
+    </>
   )
 }
 
--- frontend/react-app/src/interface/index.ts
+++ frontend/react-app/src/interface/index.ts
@@ -1,3 +1,29 @@
+export interface SignUpParams {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+export interface User {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  name: string
+  nickname?: string
+  image?: {
+    url: string
+  }
+  allowPasswordChange: boolean
+  createdAt?: any
+  updatedAt?: any
+}
 export interface Blog {
   id: string
   title: string
