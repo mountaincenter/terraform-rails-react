375913ee59eb5c16d8e9fb944869a377ae66c78b
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-08 23:01:50 +0900

    modify

--- terraform/ecs_rails/main.tf
+++ terraform/ecs_rails/main.tf
@@ -4,6 +4,7 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy_arn = data.aws_iam_policy.ecs_task_execution_role_policy.arn
 }
+
 resource "aws_ecs_task_definition" "main" {
   family                   = var.app_name
   cpu                      = 256
@@ -13,10 +14,12 @@ resource "aws_ecs_task_definition" "main" {
   container_definitions    = data.template_file.container_definitions.rendered
   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
+
 resource "aws_cloudwatch_log_group" "log" {
   count = length(var.apps_name)
   name  = "/ecs/${var.app_name}/${var.apps_name[count.index]}"
 }
+
 resource "aws_lb_target_group" "main" {
   name        = var.app_name
   vpc_id      = var.vpc_id
@@ -28,6 +31,7 @@ resource "aws_lb_target_group" "main" {
     path = "/"
   }
 }
+
 resource "aws_lb_listener_rule" "http_rule" {
   listener_arn = var.http_listener_arn
   action {
@@ -40,6 +44,7 @@ resource "aws_lb_listener_rule" "http_rule" {
     }
   }
 }
+
 resource "aws_lb_listener_rule" "https_rule" {
   listener_arn = var.https_listener_arn
   action {
@@ -52,6 +57,7 @@ resource "aws_lb_listener_rule" "https_rule" {
     }
   }
 }
+
 resource "aws_ecs_service" "main" {
   name            = "${var.app_name}-service"
   launch_type     = "FARGATE"
